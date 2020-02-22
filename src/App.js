94672253246fe07916da4376d940b4e661acc89c
Nandini <nandinig819@gmail.com>
2020-02-22 23:54:56 +0530

    End of section 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import EventDashboard from './features/events/EventDashboard/EventDashboard';
+import NavBar from './features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div >
-     <h1>hi</h1>
-    </div>
+    <Fragment>
+    <NavBar/>
+    <Container className="main">
+     
+    <EventDashboard/>
+
+    </Container>
+    </Fragment>
   );
 }
 
